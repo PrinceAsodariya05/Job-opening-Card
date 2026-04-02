59fe37e68850fc6f2b6532a0f998c7cb833cc641
Prince <asodariyaprince05@gmail.com>
2026-04-02 13:13:10 +0530

    Adding first file

---
+++ first.py
@@ -0,0 +1,17 @@
+# a = int(input("Enter a number: "))
+# b = int(input("Enter a number: "))
+
+# print(a**3)
+
+# name = input("Enter your name: ")
+# namesort = name[2:20:2]
+# print(namesort)
+# letter = '''  
+# Dear <|Name|>, 
+# You are selected! 
+# <|Date|> '''
+# print(letter.replace("<|Name|>", "Prince").replace("<|Date|>", "10th Oct 2026"))
+
+name = "weite something that has two  double  spaces"
+print(name.replace("  ", " "))
+print(name)
