3b76e37257e4a4fe748489101c111409da04bdfd
Prince <asodariyaprince05@gmail.com>
2026-04-02 14:38:17 +0530

    adding component

--- Reactshot/src/App.jsx
+++ Reactshot/src/App.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
+import Card from './component/card'
 
 const App = () => {
   return (
-    <>
-    <div className='dholu'>
-      <h1>Hii im trying to make react app</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, officia?</p>
-
+  
+    <div>
+      <Card/>
+      
     </div>
-    <div className='bholu'>
-      <h1>i guess its working</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consequatur ipsam error esse minima veritatis quis officiis doloribus officia fugiat.</p>
-
-    </div>
-    </>
+    
+    
   )
 }
 
---
+++ Reactshot/src/component/card.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const one = () => {
+  return (
+    <>
+    <div className='dholu'>
+      <h1>Hii im trying to make react app</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, officia?</p>
+
+    </div>
+    <div className='bholu'>
+      <h1>i guess its working</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consequatur ipsam error esse minima veritatis quis officiis doloribus officia fugiat.</p>
+
+    </div>
+    </>
+  )
+}
+
+export default one
