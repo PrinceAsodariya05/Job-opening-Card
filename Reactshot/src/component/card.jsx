3cfe2fb455272ea4dca937eda64ff4554fa14057
Prince <asodariyaprince05@gmail.com>
2026-04-06 17:53:42 +0530

    card added

--- Reactshot/src/component/card.jsx
+++ Reactshot/src/component/card.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
-const one = () => {
+const one = (props) => {
   return (
-    <>
-    <div className='dholu'>
-      <h1>Hii im trying to make react app</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, officia?</p>
-
+    <div className='cards-container'>
+      <div className='dholu'>
+        <img src={props.img} alt="" />
+        <h1>{props.user},{props.age}</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, officia?</p>
+      </div>
+      <div className='bholu'>
+        <h1>i guess its working</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consequatur ipsam error esse minima veritatis quis officiis doloribus officia fugiat.</p>
+        <button>Enter to visit nothing</button>
+      </div>
     </div>
-    <div className='bholu'>
-      <h1>i guess its working</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consequatur ipsam error esse minima veritatis quis officiis doloribus officia fugiat.</p>
-
-    </div>
-    </>
   )
 }
 
---
+++ Reactshot/src/component/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav'> 
+        <h1>i want to add a nav bar</h1>
+    </div>
+  )
+}
+
+export default Navbar
